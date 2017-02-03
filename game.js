345e2ed16d2c5a37a796adf43849dc8322da17bf
Andrew Ray <alr2569@gmail.com>
2017-02-03 15:41:26 -0500

    Initial Commit

---
+++ game.js
@@ -0,0 +1,192 @@
+// game.js for Perlenspiel 3.2.x
+
+/* jshint -W097 */
+
+/*
+Perlenspiel is a scheme by Professor Moriarty (bmoriarty@wpi.edu).
+Perlenspiel is Copyright © 2009-17 Worcester Polytechnic Institute.
+This file is part of Perlenspiel.
+
+Perlenspiel is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Perlenspiel is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+
+You may have received a copy of the GNU Lesser General Public License
+along with Perlenspiel. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// The "use strict" directive in the following line is important. Don't remove it!
+"use strict";
+
+// The following comment lines are for JSLint/JSHint. Don't remove them!
+
+/*jslint nomen: true, white: true */
+/*global PS: false, gameMode: true */
+
+// This is a template for creating new Perlenspiel games
+
+// All of the functions below MUST exist, or the engine will complain!
+
+// PS.init( system, options )
+// Initializes the game
+// This function should normally begin with a call to PS.gridSize( x, y )
+// where x and y are the desired initial dimensions of the grid
+// [system] = an object containing engine and platform information; see documentation for details
+// [options] = an object with optional parameters; see documentation for details
+
+var images = {};
+
+var WIDTH = 32;
+var HEIGHT = 32;
+
+PS.init = function( system, options ) {
+	PS.gridSize( WIDTH, HEIGHT );
+	PS.border(PS.ALL, PS.ALL, 0);
+	PS.gridColor(0x999999);
+	// Add any other initialization code you need here
+	
+	PS.statusText("...");
+	
+	putImage("imgs/battle.png");
+	putImage("imgs/thoth.png", 5, 17);
+	putImage("imgs/healthbar/health15.png", 0, 3);
+	putImage("imgs/healthbar/health2.png", 29, 3);
+};
+
+function putImage (filename, x1, y1, x2, y2) {
+	x1 = x1 || 0;
+	y1 = y1 || 0;
+	x2 = x2 || WIDTH;
+	y2 = y2 || HEIGHT;
+	
+	var draw = function (image) {
+		PS.imageBlit(image, x1, y1, {left: 0, right: 0, width: x2 - x1, height: y2 - y1});
+	};
+	
+	if (images[filename]) {
+		draw(images[filename]);
+	}
+	else {
+		PS.imageLoad(filename, draw);
+	}
+}
+
+// PS.touch ( x, y, data, options )
+// Called when the mouse button is clicked on a bead, or when a bead is touched
+// It doesn't have to do anything
+// [x] = zero-based x-position of the bead on the grid
+// [y] = zero-based y-position of the bead on the grid
+// [data] = the data value associated with this bead, 0 if none has been set
+// [options] = an object with optional parameters; see documentation for details
+
+PS.touch = function( x, y, data, options ) {
+	// Uncomment the following line to inspect parameters
+	// PS.debug( "PS.touch() @ " + x + ", " + y + "\n" );
+
+	// Add code here for mouse clicks/touches over a bead
+};
+
+// PS.release ( x, y, data, options )
+// Called when the mouse button is released over a bead, or when a touch is lifted off a bead
+// It doesn't have to do anything
+// [x] = zero-based x-position of the bead on the grid
+// [y] = zero-based y-position of the bead on the grid
+// [data] = the data value associated with this bead, 0 if none has been set
+// [options] = an object with optional parameters; see documentation for details
+
+PS.release = function( x, y, data, options ) {
+	// Uncomment the following line to inspect parameters
+	// PS.debug( "PS.release() @ " + x + ", " + y + "\n" );
+
+	// Add code here for when the mouse button/touch is released over a bead
+};
+
+// PS.enter ( x, y, button, data, options )
+// Called when the mouse/touch enters a bead
+// It doesn't have to do anything
+// [x] = zero-based x-position of the bead on the grid
+// [y] = zero-based y-position of the bead on the grid
+// [data] = the data value associated with this bead, 0 if none has been set
+// [options] = an object with optional parameters; see documentation for details
+
+PS.enter = function( x, y, data, options ) {
+	// Uncomment the following line to inspect parameters
+	// PS.debug( "PS.enter() @ " + x + ", " + y + "\n" );
+
+	// Add code here for when the mouse cursor/touch enters a bead
+};
+
+// PS.exit ( x, y, data, options )
+// Called when the mouse cursor/touch exits a bead
+// It doesn't have to do anything
+// [x] = zero-based x-position of the bead on the grid
+// [y] = zero-based y-position of the bead on the grid
+// [data] = the data value associated with this bead, 0 if none has been set
+// [options] = an object with optional parameters; see documentation for details
+
+PS.exit = function( x, y, data, options ) {
+	// Uncomment the following line to inspect parameters
+	// PS.debug( "PS.exit() @ " + x + ", " + y + "\n" );
+
+	// Add code here for when the mouse cursor/touch exits a bead
+};
+
+// PS.exitGrid ( options )
+// Called when the mouse cursor/touch exits the grid perimeter
+// It doesn't have to do anything
+// [options] = an object with optional parameters; see documentation for details
+
+PS.exitGrid = function( options ) {
+	// Uncomment the following line to verify operation
+	// PS.debug( "PS.exitGrid() called\n" );
+
+	// Add code here for when the mouse cursor/touch moves off the grid
+};
+
+// PS.keyDown ( key, shift, ctrl, options )
+// Called when a key on the keyboard is pressed
+// It doesn't have to do anything
+// [key] = ASCII code of the pressed key, or one of the PS.KEY constants documented at:
+// http://users.wpi.edu/~bmoriarty/ps/constants.html
+// [shift] = true if shift key is held down, else false
+// [ctrl] = true if control key is held down, else false
+// [options] = an object with optional parameters; see documentation for details
+
+PS.keyDown = function( key, shift, ctrl, options ) {
+	// Uncomment the following line to inspect parameters
+	//	PS.debug( "DOWN: key = " + key + ", shift = " + shift + "\n" );
+
+	// Add code here for when a key is pressed
+};
+
+// PS.keyUp ( key, shift, ctrl, options )
+// Called when a key on the keyboard is released
+// It doesn't have to do anything
+// [key] = ASCII code of the pressed key, or one of the PS.KEY constants documented at:
+// http://users.wpi.edu/~bmoriarty/ps/constants.html
+// [shift] = true if shift key is held down, false otherwise
+// [ctrl] = true if control key is held down, false otherwise
+// [options] = an object with optional parameters; see documentation for details
+
+PS.keyUp = function( key, shift, ctrl, options ) {
+	// Uncomment the following line to inspect parameters
+	// PS.debug( "PS.keyUp(): key = " + key + ", shift = " + shift + ", ctrl = " + ctrl + "\n" );
+
+	// Add code here for when a key is released
+};
+
+// PS.shutdown ( options )
+// Called when the browser window running Perlenspiel is about to close
+// It doesn't have to do anything
+// [options] = an object with optional parameters; see documentation for details
+
+PS.shutdown = function( options ) {
+
+	// Add code here for when Perlenspiel is about to close
+};
---
+++ modes.js
@@ -0,0 +1,27 @@
+/* global PS: false */
+
+var gameMode;
+
+var BATTLEMODE = {
+	enterMode: function (lastState) {
+		this.lastState = lastState;
+	},
+	click: function (x, y, data, options) {
+		
+	},
+	release: function (x, y, data, options) {
+		
+	},
+	enterBead: function (x, y, data, options) {
+		
+	},
+	exitBead: function (x, y, data, options) {
+		
+	},
+	keyDown: function (key, shift, ctrl, options) {
+		
+	},
+	keyUp: function (key, shift, ctrl, options) {
+		
+	}
+};
